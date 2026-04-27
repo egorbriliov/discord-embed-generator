74d305667e92e941ba450eb29e621ff2767162d1
Egor Shashkun <stas.basov@whatever.com>
2026-04-27 11:32:19 +0300

    Change Token usage and document on_ready().

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import disnake
 from disnake.ext import commands
 
-from app.token_get import token
+from utils.enviroment import Env
 
 bot = commands.InteractionBot(
     reload=True,
@@ -12,10 +12,11 @@ bot = commands.InteractionBot(
 
 @bot.event
 async def on_ready():
-    print(f'\033[3;32mБот \033[0m\033[35;1m{bot.user.name}\033[0m\033[3;32m запустился и готов к '
-          f'использованию!\033[0m')
+    """Ассинхронная функция запуска проекта."""
+    print(f'\033[3;32mБот \033[0m\033[35;1m{bot.user.name}\033[0m\033[3;32m \
+          запустился и готов к использованию!\033[0m')
 
 
 bot.load_extensions("cogs/bot/slash_commands")
 
-bot.run(token(name="TOKEN"))
+bot.run(Env.TOKEN)
